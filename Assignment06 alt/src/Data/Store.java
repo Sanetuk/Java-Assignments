84b65a3416ac6b4f45e7b40822edbc4e94e682ef
Sanetuk <yng.uk.kim@gmail.com>
2024-07-09 08:56:16 +0900

    Add files via upload

---
+++ Assignment06 alt/src/Data/Store.java
@@ -0,0 +1,78 @@
+package Data;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Store {
+	
+	// 상점 이름
+	public String name;
+	
+	
+	// 고객 목록
+	public ArrayList<Customer> customers = new ArrayList<Customer>();
+
+	
+	// 제품 목록
+	public ArrayList<Product> products = new ArrayList<Product>();
+	
+	
+	// 오늘 팔린 물건들 리스트
+	public ArrayList<Product> todayProducts = new ArrayList<Product>();
+	
+	
+	// 오늘 총매출
+	public int todayTotalSold;
+
+		
+
+	
+	// 오늘 팔린 물건들 리스트를 추가할 해시
+	public HashMap<String, ArrayList<Product>> dayToSoldProductList = new HashMap<String, ArrayList<Product>>();
+	
+	// 오늘 팔린 총 금액을 추가할 해시
+	public HashMap<String, Integer> dayToTotal = new HashMap<String, Integer>();
+	
+	// 오늘 날짜
+	Date date1 = new Date();
+	SimpleDateFormat format1 = new SimpleDateFormat("YYYY-MM-dd");
+	String today = format1.format(date1);
+	
+	// 함수 pay 정의
+	public void pay(Customer customer, Product product) {
+		
+		// 날짜 체크
+		
+		
+		// 고객 안에 내가 산 제품에 추가
+		customer.payBoughtProducts(product);
+		
+		// 내가 산 총 금액에 합산
+		customer.totalPaid += product.price;
+		
+		// 제품의 팔린 전체 금액에 합산
+		product.totalSold += product.price;
+		
+		// 팔린 물건 리스트에 저장
+		todayProducts.add(product);
+		
+		// 하루 총 매출 갱신
+		todayTotalSold += product.price;
+		
+		
+	}
+	
+	// 해시에 리스트를 추가하는 함수
+	public void balanceProduct() {
+		dayToSoldProductList.put(today, todayProducts);
+	}
+	
+	// 해시에 총매출을 추가하는 함수
+	public void balanceSold() {
+		dayToTotal.put(today, todayTotalSold);
+	}
+	
+	
+}
---
+++ Assignment06 alt/src/Test.java
@@ -0,0 +1,79 @@
+
+import Data.Customer;
+import Data.Product;
+import Data.Store;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		// 매장 찍어내기
+		Store store1 = new Store();
+		store1.name = "Mattel";
+		
+		// 고객 찍어내기
+		Customer customer1 = new Customer("Ken", 63, "111-1111-1111");
+		store1.customers.add(customer1);
+		
+		Customer customer2 = new Customer("Barbie", 65, "222-2222-2222");
+		store1.customers.add(customer2);
+		
+		Customer customer3 = new Customer("Allen", 60, "333-3333-3333");
+		store1.customers.add(customer3);
+		
+		// 제품 찍어내기
+		Product product1 = new Product("Milk", 2000);
+		store1.products.add(product1);
+		
+		Product product2 = new Product("Car", 3000);
+		store1.products.add(product2);
+		
+		Product product3 = new Product("Horse", 4000);
+		store1.products.add(product3);
+		
+		
+		// 매장 안에 들어있는 고객의 숫자, 제품의 숫자 출력
+		System.out.println("매장 안 고객의 숫자 : " + store1.customers.size());
+		System.out.println("매장 안 제품의 숫자 : " + store1.products.size());
+		
+
+		
+		
+		// 고객이 물건 구매(pay 함수 사용)
+		store1.pay(customer1, product1);
+		store1.pay(customer1, product3);
+		store1.pay(customer2, product1);
+		store1.pay(customer3, product2);
+		store1.pay(customer3, product1);
+		
+		
+		
+		// 하루 정산
+		store1.balanceProduct();
+		store1.balanceSold();
+		
+		
+		
+		// 출력
+		for (String date : store1.dayToSoldProductList.keySet()) {
+			System.out.println("해시 키 : " + date);
+		}
+
+
+		for (String date : store1.dayToTotal.keySet()) {
+			System.out.println("해시 키 : " + date);
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+	
+	
